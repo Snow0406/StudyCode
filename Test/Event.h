191abef0e5bcdbca1a5600244485e388e6015b9c
Snow <87596507+Snow0406@users.noreply.github.com>
2024-05-09 00:51:04 +0900

    Update Event.h

--- Test/Event.h
+++ Test/Event.h
@@ -23,9 +23,9 @@ private:
 	void Shuffle();
 
 private:
-	std::vector<std::string> user = { "화초", "농보", "그린", "스노우", "유겸", "민호", "환수", "여노", "빌드", "메구밍",
-	"ggcollee246", "테니", "졸려", "호두나무", "승기", "샌즈", "뱁새", "캔아다", "앱"};
-	std::vector<std::string> boost = { "앱", "메구밍", "스노우", "환수"};
+	std::vector<std::string> user = { "화초", "농보", "사로로", "스노우", "민호", "빌드", "메구밍", "유경", 
+		"gg", "졸려", "호두나무", "승기", "샌즈", "닉네임", "하윤", "뱁새", "노는앱", "Viento", "테니"};
+	std::vector<std::string> boost = { "앱", "스노우"};
 	std::vector<std::string> list = {};
 };
 
